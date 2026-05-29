7696b346088f360a24a197958ef05a4754c18efb
Juan David Diaz Perez <jddiazp@udistrital.edu.co>
2026-05-29 18:53:12 -0500

    Modelo refactorizacion, refactorizacion de comentario de clase del ZoneFactory

--- src/co/edu/udistrital/model/enums/ZoneFactory.java
+++ src/co/edu/udistrital/model/enums/ZoneFactory.java
@@ -1,8 +1,8 @@
 package co.edu.udistrital.model.enums;
 
 /**
- * Clase encargada de recibir que tipo de enum de tipo zona usar en la logica
- * del sistema
+ * Clase encargada de retornar que tipo de enum de tipo zona usar en la logica
+ * del sistema con base en la seleccion de la vista
  *
  * @author Juan David Diaz Peres
  */
