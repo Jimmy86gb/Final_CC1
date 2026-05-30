bfbb5fe8e41bf8c15b91865c663898b36fd12ce2
Juan David Diaz Perez <jddiazp@udistrital.edu.co>
2026-05-29 19:16:20 -0500

    Modelo implementacion, implementacion de elTechnicianFactory para apoyar los casos de uso

---
+++ src/co/edu/udistrital/model/enums/TechnicianFactory.java
@@ -0,0 +1,28 @@
+package co.edu.udistrital.model.enums;
+
+/**
+ * Clase encargada de retornar que tipo de enum de tipo de especialidad del
+ * tecnico a usar en la logica del sistema con base en la seleccion de la vista
+ *
+ * @author Juan David Diaz Perez
+ */
+public class TechnicianFactory {
+
+	/**
+	 * Metodo que recibe el tipo de especialidad del tecnico en string y retorna su
+	 * equivalente en enum
+	 * 
+	 * @param technicianSpeciality Especialidad en string del tenico
+	 * @return Especialidad en enum del tecnico
+	 */
+	public TechnicianSpecialty generaTechnicianSpecialty(String technicianSpeciality) {
+		return switch (technicianSpeciality.toLowerCase().trim()) {
+		case "electricoautomotriz" -> TechnicianSpecialty.AUTOMOTIVE_ELECTRICITY;
+		case "operadordegrua" -> TechnicianSpecialty.CRANE_OPERATION;
+		case "mecanicogeneral" -> TechnicianSpecialty.GENERAL_MECHANICS;
+		case "operariomontallantas" -> TechnicianSpecialty.PLUMBING;
+		case "cerrajerodevehiculos" -> TechnicianSpecialty.VEHICULAR_LOCKSMITH;
+		default -> throw new IllegalArgumentException("Tipo de especialidad no soportado: " + technicianSpeciality);
+		};
+	}
+}
