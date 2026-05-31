047b2077018b1116b61a5be2c09663226e85f795
Juan David Diaz Perez <jddiazp@udistrital.edu.co>
2026-05-30 22:10:16 -0500

    Modelo implementación, actualización del enum TechnicianStatus para incluir nombres de visualización

--- src/co/edu/udistrital/model/enums/TechnicianStatus.java
+++ src/co/edu/udistrital/model/enums/TechnicianStatus.java
@@ -1,5 +1,18 @@
 package co.edu.udistrital.model.enums;
 
 public enum TechnicianStatus {
-	AVAILABLE, BUSY, INACTIVE
+	AVAILABLE("Disponible"), BUSY("Ocupado"), INACTIVE("Inactivo");
+
+	private final String displayName;
+
+	private TechnicianStatus(String displayName) {
+		this.displayName = displayName;
+	}
+
+	/**
+	 * @return the displayName
+	 */
+	public String getDisplayName() {
+		return displayName;
+	}
 }
