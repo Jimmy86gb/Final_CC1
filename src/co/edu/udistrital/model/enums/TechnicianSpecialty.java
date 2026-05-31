8328187e6a3a104bb4ea528d289916286678b929
Juan David Diaz Perez <jddiazp@udistrital.edu.co>
2026-05-30 22:07:49 -0500

    Modelo implementación, actualización del enum TechnicianSpecialty para incluir nombres de visualización

--- src/co/edu/udistrital/model/enums/TechnicianSpecialty.java
+++ src/co/edu/udistrital/model/enums/TechnicianSpecialty.java
@@ -1,5 +1,20 @@
 package co.edu.udistrital.model.enums;
 
 public enum TechnicianSpecialty {
-	GENERAL_MECHANICS, AUTOMOTIVE_ELECTRICITY, VEHICULAR_LOCKSMITH, CRANE_OPERATION, PLUMBING
+	GENERAL_MECHANICS("Mecanico General"), AUTOMOTIVE_ELECTRICITY("Electrico Automotriz"),
+	VEHICULAR_LOCKSMITH("Cerrajero de Vehiculos"), CRANE_OPERATION("Operador de Grua"),
+	PLUMBING("Operario Montallantas");
+
+	private final String displayName;
+
+	private TechnicianSpecialty(String displayName) {
+		this.displayName = displayName;
+	}
+
+	/**
+	 * @return the displayName
+	 */
+	public String getDisplayName() {
+		return displayName;
+	}
 }
