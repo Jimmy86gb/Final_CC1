d830a51ad9a1f0a5359d89bd08a710d77d648a12
Juan David Diaz Perez <jddiazp@udistrital.edu.co>
2026-05-30 21:59:56 -0500

    Modelo implementación, actualización del enum KitType para incluir nombres de visualización

--- src/co/edu/udistrital/model/enums/KitType.java
+++ src/co/edu/udistrital/model/enums/KitType.java
@@ -1,5 +1,19 @@
 package co.edu.udistrital.model.enums;
 
 public enum KitType {
-	PLUMBING_KIT, ELECTRICITY_KIT, LOCKSMITH_KIT, CRANE_KIT, GENERAL_KIT
+	PLUMBING_KIT("Kit de Montallantas"), ELECTRICITY_KIT("Kit de Electricidad"), LOCKSMITH_KIT("Kit de Cerrajeria"),
+	CRANE_KIT("Kit de Grua"), GENERAL_KIT("Kit General");
+
+	private final String displayName;
+
+	private KitType(String displayName) {
+		this.displayName = displayName;
+	}
+
+	/**
+	 * @return the displayName
+	 */
+	public String getDisplayName() {
+		return displayName;
+	}
 }
