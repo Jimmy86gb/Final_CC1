77a92ccfad0bb7a188f67ff36ce81c9eebe57d37
Juan David Diaz Perez <jddiazp@udistrital.edu.co>
2026-05-31 22:22:00 -0500

    Modelo implementacion, implementacion del repositorio de perfiles con los usuarios base

---
+++ src/co/edu/udistrital/model/repositories/ProfileRepository.java
@@ -0,0 +1,41 @@
+package co.edu.udistrital.model.repositories;
+
+import co.edu.udistrital.model.entities.Profile;
+import co.edu.udistrital.model.enums.ProfileType;
+import co.edu.udistrital.model.structures.SimpleList;
+import co.edu.udistrital.model.structures.SimpleList.Iterator;
+
+public class ProfileRepository {
+
+	/**
+	 * Lista que contiene la lista de usuarios en el sistema
+	 */
+	private final SimpleList<Profile> usersList = new SimpleList<>();
+
+	/**
+	 * Constructor que inicializa los usuarios base del sistema
+	 */
+	public ProfileRepository() {
+		usersList.add(new Profile("admin", "admin1234", ProfileType.ADMIN));
+		usersList.add(new Profile("operario", "operario1234", ProfileType.OPERATOR));
+	}
+
+	/**
+	 * Metodo que verifica si el usuario ingresado esta dentro de los casos base
+	 * 
+	 * @param username Usuario ingresado
+	 * @param password Contraseña ingresada
+	 * @return El perfil si existe dentro del sistema
+	 */
+	public Profile authenticate(String username, String password) {
+		Iterator<Profile> iterator = usersList.iterador();
+
+		while (iterator.hasNext()) {
+			Profile current = iterator.Next();
+			if (current.getUsername().equals(username) && current.getPassword().equals(password)) {
+				return current;
+			}
+		}
+		return null;
+	}
+}
