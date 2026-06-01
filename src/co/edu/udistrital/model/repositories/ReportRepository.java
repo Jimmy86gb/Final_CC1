310115087343aa46c5ebe7a53bada8cd178b1847
Juan David Diaz Perez <jddiazp@udistrital.edu.co>
2026-06-01 16:53:09 -0500

    Modelo implementacion, implementacion del metodo de busqueda y retorno de un elemento por ID en las colas de prioridad

--- src/co/edu/udistrital/model/repositories/ReportRepository.java
+++ src/co/edu/udistrital/model/repositories/ReportRepository.java
@@ -364,4 +364,60 @@ public class ReportRepository {
 		target = extractFromQueue(lowPriorityQueue, ticketID);
 		return target;
 	}
+
+	/**
+	 * Metodo que saca de la cola especificada un reporte segun si coincide su ID
+	 * 
+	 * @param originalQueue La cola original evaluada
+	 * @param targetID      El id del reporte a buscar
+	 * @return El elemento fuera de sus colas de prioridad
+	 */
+	private Report extractFromQueue(Queue<Report> originalQueue, UUID targetID) {
+		Queue<Report> tempQueue = new Queue<>();
+		Report foundReport = null;
+
+		// 1. Vaciamos la cola original buscando el objetivo
+		while (!originalQueue.isEmpty()) {
+			Report current = originalQueue.dequeue();
+
+			if (current.getTicketID().equals(targetID)) {
+				foundReport = current; // ¡Lo encontramos! No lo metemos en la temporal
+			} else {
+				tempQueue.enqueue(current); // Los demás se guardan para no perderlos
+			}
+		}
+
+		// 2. Devolvemos los elementos a la cola original manteniendo el orden
+		while (!tempQueue.isEmpty()) {
+			originalQueue.enqueue(tempQueue.dequeue());
+		}
+
+		return foundReport;
+	}
+
+	/**
+	 * Busca un reporte específico en las colas de prioridad y lo extrae.
+	 * 
+	 * @param ticketID ID del reporte buscado.
+	 * @return El reporte si fue encontrado, o null si no estaba en ninguna cola.
+	 */
+	public Report extractPendingReport(UUID ticketID) {
+		Report target = extractFromQueue(undoQueue, ticketID);
+		if (target != null) {
+			return target;
+		}
+
+		target = extractFromQueue(highPriorityQueue, ticketID);
+		if (target != null) {
+			return target;
+		}
+
+		target = extractFromQueue(mediumPriorityQueue, ticketID);
+		if (target != null) {
+			return target;
+		}
+
+		target = extractFromQueue(lowPriorityQueue, ticketID);
+		return target;
+	}
 }
