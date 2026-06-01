c3a9c4d5616af37bdc99465fd68257f449585d8f
Juan David Diaz Perez <jddiazp@udistrital.edu.co>
2026-05-31 23:12:26 -0500

    Modelo correccion, correccion del nombre del metodo getByID del repositorio de tecnicos

--- src/co/edu/udistrital/model/repositories/TechnicianRepository.java
+++ src/co/edu/udistrital/model/repositories/TechnicianRepository.java
@@ -62,9 +62,9 @@ public class TechnicianRepository {
 	 * busqueda por ID
 	 * 
 	 * @param id Id para buscar la lista
-	 * @return Elemento cliente con la id dada
+	 * @return Elemento tecnico con la id dada
 	 */
-	public Technician getClientByID(UUID id) {
+	public Technician getTechnicianByID(UUID id) {
 		Iterator<Technician> iterator = this.technicianList.iterador();
 
 		while (iterator.hasNext()) {
