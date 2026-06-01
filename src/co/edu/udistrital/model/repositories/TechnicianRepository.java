f859811b329fa71f03175cbd9284318cacb81d5a
Juan David Diaz Perez <jddiazp@udistrital.edu.co>
2026-06-01 00:37:53 -0500

    Modelo correccion, correccion de comparaciones entre UUID en los repositorios

--- src/co/edu/udistrital/model/repositories/TechnicianRepository.java
+++ src/co/edu/udistrital/model/repositories/TechnicianRepository.java
@@ -60,7 +60,7 @@ public class TechnicianRepository {
 		while (iterator.hasNext()) {
 			Technician currentTechnician = iterator.Next();
 
-			if (currentTechnician.getId() == id) {
+			if (currentTechnician.getId().equals(id)) {
 				return currentTechnician;
 			}
 		}
