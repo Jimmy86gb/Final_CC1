6f8270aad42312618150a9296a67411d9bd79b84
Juan David Diaz Perez <jddiazp@udistrital.edu.co>
2026-05-31 22:18:36 -0500

    Modelo implementacion, implementacion de la entidad del perfil del sistema

---
+++ src/co/edu/udistrital/model/entities/Profile.java
@@ -0,0 +1,51 @@
+package co.edu.udistrital.model.entities;
+
+import co.edu.udistrital.model.enums.ProfileType;
+
+/**
+ * Clase que representa el perfil estatico dentro del sistema
+ *
+ * @author Juan David Diaz Perez
+ */
+public class Profile {
+
+	private final String username;
+	private final String password;
+	private final ProfileType profileType;
+
+	/**
+	 * Constructor de la entidad perfil que inicializa los datos en el sistema
+	 * 
+	 * @param username    Usuario del perfil actual
+	 * @param password    Contraseña del perfil actual
+	 * @param profileType Tipo del perfil actual
+	 */
+	public Profile(String username, String password, ProfileType profileType) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.profileType = profileType;
+	}
+
+	/**
+	 * @return Usuario del perfil actual
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * @return Contraseña del perfil actual
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @return Tipo del perfil actual
+	 */
+	public ProfileType getProfileType() {
+		return profileType;
+	}
+
+}
