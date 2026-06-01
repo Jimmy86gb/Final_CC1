aeb5deba8b0cf5dad16f450679cc6e312731d663
Juan David Diaz Perez <jddiazp@udistrital.edu.co>
2026-05-31 23:20:12 -0500

    Modelo implementacion, implementacion del factory del status de las unidades de servicio

---
+++ src/co/edu/udistrital/model/enums/UnitStatusFactory.java
@@ -0,0 +1,27 @@
+package co.edu.udistrital.model.enums;
+
+/**
+ * Clase encargada de retornar que tipo de enum de tipo de status de la unidad
+ * de servicio a usar en la logica del sistema con base en la seleccion de la
+ * vista
+ *
+ * @author Juan David Diaz Perez
+ */
+public class UnitStatusFactory {
+
+	/**
+	 * Metodo que recibe el status de la unidad de servicio en string y retorna su
+	 * equivalente en enum
+	 * 
+	 * @param unitStatus Status en string
+	 * @return Su equivalente en ENUM
+	 */
+	public UnitStatus generateUnitStatus(String unitStatus) {
+		return switch (unitStatus.toLowerCase().trim()) {
+		case "disponible" -> UnitStatus.AVAILABLE;
+		case "asignada" -> UnitStatus.ASSIGNED;
+		case "enmantenimiento" -> UnitStatus.MAINTENANCE;
+		default -> throw new IllegalArgumentException("Tipo de status no soportado: " + unitStatus);
+		};
+	}
+}
