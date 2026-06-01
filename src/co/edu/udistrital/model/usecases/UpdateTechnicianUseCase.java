2db3cbe9bf59b098d374bd830da1aaa2d823ff4e
Juan David Diaz Perez <jddiazp@udistrital.edu.co>
2026-05-31 23:04:43 -0500

    Modelo implementacion, implementacion del caso de uso de actualizacion de informacion de tecnicos

---
+++ src/co/edu/udistrital/model/usecases/UpdateTechnicianUseCase.java
@@ -0,0 +1,93 @@
+package co.edu.udistrital.model.usecases;
+
+import java.util.UUID;
+
+import co.edu.udistrital.model.dtos.ResponseDTO;
+import co.edu.udistrital.model.entities.Technician;
+import co.edu.udistrital.model.enums.OperationZone;
+import co.edu.udistrital.model.enums.TechnicianFactory;
+import co.edu.udistrital.model.enums.TechnicianSpecialty;
+import co.edu.udistrital.model.enums.TechnicianStatus;
+import co.edu.udistrital.model.enums.TechnicianStatusFactory;
+import co.edu.udistrital.model.enums.ZoneFactory;
+import co.edu.udistrital.model.repositories.TechnicianRepository;
+
+/**
+ * Clase de caso de uso que se encarga de actualizar la informacion de los
+ * tecnicos recibida desde la vista
+ *
+ * @author Juan David Diaz Perez
+ */
+public class UpdateTechnicianUseCase {
+
+	/**
+	 * Instancia del factory de la especializacion del tecnico
+	 */
+	private final TechnicianFactory technicianFactory = new TechnicianFactory();
+
+	/**
+	 * Instancia del factory de la zona de las entidades
+	 */
+	private final ZoneFactory zoneFactory = new ZoneFactory();
+
+	/**
+	 * Instancia del factory de el status del tecnico
+	 */
+	private final TechnicianStatusFactory technicianStatusFactory = new TechnicianStatusFactory();
+
+	/**
+	 * Instancia del repositorio que maneja la memoria de los técnicos
+	 */
+	private final TechnicianRepository technicianRepository;
+
+	/**
+	 * Constructor que inyecta el repositorio centralizado del sistema
+	 * 
+	 * @param technicianRepository El DAO de técnicos instanciado en el sistema
+	 */
+	public UpdateTechnicianUseCase(TechnicianRepository technicianRepository) {
+		this.technicianRepository = technicianRepository;
+	}
+
+	/**
+	 * Metodo que ejecuta el caso de uso de actualizacion de datos de tenico
+	 * 
+	 * @param id        El ID de ambos tecnicos
+	 * @param name      El nombre de ambos tecnicos
+	 * @param specialty La especialidad de ambos tecnicos
+	 * @param zone      La nueva zona del tecnico
+	 * @param status    El nuevo status del tecnico
+	 * @return DTO de respuesta frente a la accion realizada
+	 */
+	public ResponseDTO execute(UUID id, String name, String specialty, String zone, String status) {
+		try {
+			TechnicianSpecialty technicianSpecialty = technicianFactory.generaTechnicianSpecialty(specialty);
+			OperationZone operationZone = zoneFactory.generateOperationZone(zone);
+			TechnicianStatus technicianStatus = technicianStatusFactory.generaTechnicianStatus(status);
+
+			Technician actualTechnician = technicianRepository.getClientByID(id);
+
+			if (actualTechnician == null) {
+				return new ResponseDTO(false,
+						"El tecnico no se encontró en el sistema. Es posible que haya sido eliminado.");
+			}
+
+			Technician newTechnician = new Technician(name, technicianSpecialty, operationZone, technicianStatus);
+
+			newTechnician.setId(id);
+
+			boolean isUpdated = technicianRepository.update(actualTechnician, newTechnician);
+
+			if (isUpdated) {
+				return new ResponseDTO(true, "La información del tecnico " + name + " fue actualizada exitosamente.");
+			} else {
+				return new ResponseDTO(false, "No se pudo actualizar la información del tecnico.");
+			}
+
+		} catch (IllegalArgumentException e) {
+			return new ResponseDTO(false, "Datos inválidos: " + e.getMessage());
+		} catch (Exception e) {
+			return new ResponseDTO(false, "Ocurrió un error inesperado al actualizar los datos del tecnico.");
+		}
+	}
+}
