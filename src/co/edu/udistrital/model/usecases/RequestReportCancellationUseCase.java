0f715e88728a8193c76d3421a907837aa753cc7f
Juan David Diaz Perez <jddiazp@udistrital.edu.co>
2026-06-01 18:38:14 -0500

    Modelo correccion, correccion de parametros execute con UUID, para pasar un string y convertirlo en el caso de uso

--- src/co/edu/udistrital/model/usecases/RequestReportCancellationUseCase.java
+++ src/co/edu/udistrital/model/usecases/RequestReportCancellationUseCase.java
@@ -33,12 +33,14 @@ public class RequestReportCancellationUseCase {
 	/**
 	 * Ejecuta la solicitud de cancelación.
 	 * 
-	 * @param ticketIdStr El ID del reporte a cancelar (enviado desde la UI como
-	 *                    String).
+	 * @param ticketId El ID del reporte a cancelar (enviado desde la UI como
+	 *                 String).
 	 * @return DTO con el resultado de la operación.
 	 */
-	public ResponseDTO execute(UUID ticketID) {
+	public ResponseDTO execute(String ticketId) {
 		try {
+
+			UUID ticketID = UUID.fromString(ticketId);
 
 			Report reportToCancel = reportRepository.extractPendingReport(ticketID);
 
