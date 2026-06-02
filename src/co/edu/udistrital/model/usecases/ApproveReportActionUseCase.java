86957ef1ae6441755023f94142c09188e1ce6c3b
Juan David Diaz Perez <jddiazp@udistrital.edu.co>
2026-06-01 22:07:06 -0500

    Modelo implementacion, implementacion del caso de uso de aprobacion de cambio de estado y finalizacion completa de los reportes

--- src/co/edu/udistrital/model/usecases/ApproveReportActionUseCase.java
+++ src/co/edu/udistrital/model/usecases/ApproveReportActionUseCase.java
@@ -1,6 +1,7 @@
 package co.edu.udistrital.model.usecases;
 
 import co.edu.udistrital.model.dtos.ResponseDTO;
+import co.edu.udistrital.model.entities.Kit;
 import co.edu.udistrital.model.entities.Report;
 import co.edu.udistrital.model.enums.ReportStatus;
 import co.edu.udistrital.model.enums.TechnicianStatus;
@@ -8,42 +9,77 @@ import co.edu.udistrital.model.enums.UnitStatus;
 import co.edu.udistrital.model.repositories.KitRepository;
 import co.edu.udistrital.model.repositories.ReportRepository;
 
+/**
+ * Caso de uso para terminar confirmar un cambio de estado de reporte y hacerle
+ * pop en la fila de confirmaciones
+ *
+ * @author Juan David Diaz Perez
+ */
 public class ApproveReportActionUseCase {
-    private final ReportRepository reportRepository;
-    private final KitRepository kitRepository;
 
-    public ApproveReportActionUseCase(ReportRepository reportRepository, KitRepository kitRepository) {
-        this.reportRepository = reportRepository;
-        this.kitRepository = kitRepository;
-    }
+	/**
+	 * Instancia privada del repositorio de memoria de reportes
+	 */
+	private final ReportRepository reportRepository;
 
-    public ResponseDTO execute() {
-        try {
-            Report topReport = reportRepository.popOnConfirm();
-            
-            if (topReport == null) {
-                return new ResponseDTO(false, "No hay reportes pendientes de confirmación.");
-            }
-            
-            if (topReport.getStatus() == ReportStatus.CANCELLED) {
-                return new ResponseDTO(true, "Cancelación aprobada y auditada.");
-            } 
-            else if (topReport.getStatus() == ReportStatus.DONE) {
-                if (topReport.getAssignedTechnician() != null) {
-                    topReport.getAssignedTechnician().setStatus(TechnicianStatus.AVAILABLE);
-                }
-                if (topReport.getAssignedUnit() != null) {
-                    topReport.getAssignedUnit().setStatus(UnitStatus.AVAILABLE);
-                }
-                if (topReport.getAssignedKit() != null) {
-                    topReport.getAssignedKit().setStatus(UnitStatus.MAINTENANCE);
-                    kitRepository.maintainKit(topReport.getAssignedKit());
-                }
-                return new ResponseDTO(true, "Trabajo aprobado. Recursos liberados y kit despachado a mantenimiento.");
-            }
-            return new ResponseDTO(false, "Estado de reporte desconocido.");
-        } catch (Exception e) {
-            return new ResponseDTO(false, "Ocurrió un error inesperado al aprobar la acción del reporte.");
-        }
-    }
+	/**
+	 * Instancia privada del repositorio de memoria de kits;
+	 */
+	private final KitRepository kitRepository;
+
+	/**
+	 * Constructor que inyecta al caso de uso todos los repositotios necesarios en
+	 * la logica
+	 * 
+	 * @param reportRepository Repositorio de reporte
+	 * @param kitRepository    Repositorio de kits
+	 */
+	public ApproveReportActionUseCase(ReportRepository reportRepository, KitRepository kitRepository) {
+		this.reportRepository = reportRepository;
+		this.kitRepository = kitRepository;
+	}
+
+	/**
+	 * Método que se encarga de guardar estados definitivamente y liberar recursos
+	 * si es un reporte terminado
+	 * 
+	 * @return DTO de muestra en la vista con el resultado de la operación.
+	 */
+	public ResponseDTO execute() {
+		try {
+
+			Report confirmReport = reportRepository.popOnConfirm();
+
+			if (confirmReport == null) {
+				return new ResponseDTO(false, "No hay reportes pendientes en la pila de confirmaciones.");
+			}
+
+			if (confirmReport.getStatus() == ReportStatus.DONE) {
+
+				if (confirmReport.getAssignedTechnician() != null) {
+					confirmReport.getAssignedTechnician().setStatus(TechnicianStatus.AVAILABLE);
+				}
+
+				if (confirmReport.getAssignedUnit() != null) {
+					confirmReport.getAssignedUnit().setStatus(UnitStatus.AVAILABLE);
+				}
+
+				if (confirmReport.getAssignedKit() != null) {
+					Kit usedKit = confirmReport.getAssignedKit();
+					usedKit.setStatus(UnitStatus.MAINTENANCE);
+					kitRepository.maintainKit(usedKit);
+				}
+
+				return new ResponseDTO(true,
+						"Reporte aprobado como DONE. Recursos liberados y Kit enviado a mantenimiento.");
+			} else if (confirmReport.getStatus() == ReportStatus.CANCELLED) {
+				return new ResponseDTO(true, "Cancelación aprobada. El siniestro ha sido archivado como CANCELLED.");
+			}
+
+			return new ResponseDTO(false, "El reporte extraído tiene un estado no reconocible para esta acción.");
+
+		} catch (Exception e) {
+			return new ResponseDTO(false, "Ocurrió un error inesperado al intentar auditar el reporte.");
+		}
+	}
 }
