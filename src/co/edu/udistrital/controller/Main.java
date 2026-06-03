a064249f36dffb2175e8af0f5a1e40771f19ac10
Juan David Diaz Perez <jddiazp@udistrital.edu.co>
2026-06-02 19:08:34 -0500

    Controller refactor, refactorizacion de la disposicion del main

--- src/co/edu/udistrital/controller/Main.java
+++ src/co/edu/udistrital/controller/Main.java
@@ -1,24 +1,14 @@
 package co.edu.udistrital.controller;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-
 /**
- * Arranca todo el programa.
- * Basicamente, JavaFX necesita esto para prender motores y que el sistema empiece a funcionar.
+ * Arranca todo el programa. Basicamente, JavaFX necesita esto para prender
+ * motores y que el sistema empiece a funcionar.
  * 
  * @author Jimmy86gb
  */
-public class Main{
-
-    
-    public static void main(String[] args) {
-    	AppController controller = new AppController();
-    	controller.run();
+public class Main {
+	public static void main(String[] args) {
+		AppController controller = new AppController();
+		controller.run();
 	}
-    	
-    	
-    
-
-   
 }
